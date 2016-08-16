60cb678947d80f500c525acb65b6531de0ab9c02
Alexandre Serviès <alex.servies@gmail.com>
2016-08-16 22:31:46 +0100

    Simple Array Sum

---
+++ src/warmup/SimpleArraySum.java
@@ -0,0 +1,24 @@
+package warmup;
+
+import java.util.Scanner;
+
+public class SimpleArraySum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int arr[] = new int[n];
+        for (int arr_i = 0; arr_i < n; arr_i++) {
+            arr[arr_i] = in.nextInt();
+        }
+
+        System.out.println(arraySum(arr));
+    }
+
+    public static int arraySum(int[] arr) {
+        int res = 0;
+        for (int n : arr) {
+            res += n;
+        }
+        return res;
+    }
+}
