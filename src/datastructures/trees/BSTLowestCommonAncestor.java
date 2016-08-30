1ca078c9c45a96fd4e591412a21458224c828e7c
Alexandre Serviès <alex.servies@gmail.com>
2016-08-30 22:38:51 +0100

    BST : Lowest common ancestor

---
+++ src/datastructures/trees/BSTLowestCommonAncestor.java
@@ -0,0 +1,13 @@
+package datastructures.trees;
+
+public class BSTLowestCommonAncestor {
+
+    static Node lca(Node root, int v1, int v2) {
+        if (v1 < root.data && v2 < root.data) {
+            return lca(root.left, v1, v2);
+        } else if (v1 > root.data && v2 > root.data) {
+            return lca(root.right, v1, v2);
+        }
+        return root;
+    }
+}
