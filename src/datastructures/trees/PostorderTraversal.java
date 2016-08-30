5f481a4c39088548c47b060ef53112a37bccc25d
Alexandre Serviès <alex.servies@gmail.com>
2016-08-30 20:43:26 +0100

    Postorder Traversal

---
+++ src/datastructures/trees/PostorderTraversal.java
@@ -0,0 +1,19 @@
+package datastructures.trees;
+
+public class PostorderTraversal {
+    class Node {
+        int data;
+        Node left;
+        Node right;
+    }
+
+    void postOrder(Node root) {
+        if (root.left != null) {
+            postOrder(root.left);
+        }
+        if (root.right != null) {
+            postOrder(root.right);
+        }
+        System.out.print(root.data + " ");
+    }
+}
