03e9d1d693c2dab3517f4483bf00ec98720c36a1
Alexandre Serviès <alex.servies@gmail.com>
2016-08-31 20:54:33 +0100

    Super Reduced String

---
+++ src/algorithms/strings/SuperReducedString.java
@@ -0,0 +1,31 @@
+package algorithms.strings;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class SuperReducedString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(reduce(s));
+    }
+
+    private static String reduce(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+        Object[] chars = stack.toArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append((char) chars[i]);
+        }
+        String result = sb.toString();
+        return result.isEmpty() ? "Empty String" : result;
+    }
+}
