ce179d6dfdc899550e0d61edb36de9d4ed6adbe9
Alexandre Serviès <alex.servies@gmail.com>
2016-09-01 04:17:11 +0100

    Removed useless code

--- src/algorithms/strings/BeautifulBinaryString.java
+++ src/algorithms/strings/BeautifulBinaryString.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class BeautifulBinaryString {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
         String B = in.next();
 
         System.out.println((B.length() - B.replaceAll("010", "").length()) / 3);
